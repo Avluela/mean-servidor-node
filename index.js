19aa68e62c3777c58e006578c33f840e3adae1de
Gretel-Barrios <gretelbarrios.dev@gmail.com>
2023-01-30 21:22:06 -0300

    add coirs

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const conectarDB = require('./config/db');
+const cors = require('cors');
 
 // Creamos el servidor
 const app = express();
 
 // Conectamos a la base de datos
 conectarDB();
+app.use(cors());
 
 // habilitamos a que nos envien json
 app.use(express.json());
